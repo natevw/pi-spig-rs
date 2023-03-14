c6aa4f5da2f16d8f35dca926bf2270377c999f64
Nathan Vander Wilt <nate@calftrail.com>
2023-03-13 21:29:23 -0700

    port from python version appears to be working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
-fn main() {
-    const N_DIGITS: usize = 20;
-    
-    let len = (10 * N_DIGITS / 3) + 1;
-    let arr = vec![2; len];
-    _ = arr;
+type OutputDigit = u8;  // increase if converting to higher (e.g. 1000) base
+type RadixDigit = u32;  // TODO: analyze when/if this could overflow
 
-    println!("Hello, world!");
+fn main() {
+    const N_DIGITS: usize = 500;
+    
+    let arr_len = (10 * N_DIGITS / 3) + 1;
+    let mut arr = vec![2 as RadixDigit; arr_len];
+    
+    let mut first_held: OutputDigit = 0;
+    let mut num_held_nines: usize = 0;
+    let mut push_for_release = |outgoing: RadixDigit| {
+        if outgoing == 9 {
+            num_held_nines += 1;
+            return
+        } else if outgoing < 9 {
+            //print!(">{},{}<", first_held, num_held_nines);
+            // HT: https://stackoverflow.com/a/35280799/179583
+            print!("{}{:9<2$}", first_held, "", num_held_nines);
+            first_held = outgoing as OutputDigit;
+        } else {
+            assert!(outgoing == 10);
+            assert!(first_held < 9);
+            //print!(">{},{}<", first_held, num_held_nines);
+            print!("{}{:0<2$}", first_held + 1, "", num_held_nines);
+            first_held = 0;
+        } 
+        num_held_nines = 0;
+    };
+    
+    for _ in 0..N_DIGITS {
+        for a_idx in 0..arr_len {
+            arr[a_idx] *= 10;
+        }
+        for a_idx in (1..arr_len).rev() {
+            // this stuff following my Python version for now
+            let i: RadixDigit = (a_idx as RadixDigit) + 1;
+            let modulo: RadixDigit = 2*i - 1;
+            let q = arr[a_idx] / modulo;
+            let r = arr[a_idx] % modulo;
+            arr[a_idx] = r;
+            arr[a_idx - 1] += q * (i - 1);
+        }
+        let q = arr[0] / 10;
+        let r = arr[0] % 10;
+        arr[0] = r;
+        push_for_release(q)
+    }
+    push_for_release(0);
+    println!("");
 }
