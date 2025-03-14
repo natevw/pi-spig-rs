8c32c202736ef17acb50c9f1c01b38698611da41
Nathan Vander Wilt <nate@calftrail.com>
2025-03-14 16:11:04 -0700

    get display component working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, io::Read, io::stdout, net::TcpListener, process};
+use std::{env, io::stdout, io::Read, net::TcpListener, process};
 
 mod display;
 mod types;
@@ -8,22 +8,23 @@ fn exit_with_usage(code: i32) -> ! {
     process::exit(code);
 }
 
-
-
 fn display_from(src: &mut impl Read) {
-  let values = std::iter::from_fn(move || {
-    let mut buffer = [0; std::mem::size_of::<types::RadixDigit>()];
-    match src.read_exact(&mut buffer) {
-        Ok(_) => Some(types::RadixDigit::from_be_bytes(buffer)),
-        Err(_) => None,
-    }
-  });
-  
-  for v in values {
-    println!("{}", v);
-  }
-}
+    let values = std::iter::from_fn(move || {
+        let mut buffer = [0; std::mem::size_of::<types::RadixDigit>()];
+        match src.read_exact(&mut buffer) {
+            Ok(_) => Some(types::RadixDigit::from_be_bytes(buffer)),
+            Err(_) => None,
+        }
+    });
 
+    let mut output_dest = stdout().lock();
+    let mut display = display::OutputDisplay::new(&mut output_dest);
+    for q in values {
+        display.push_for_release(q);
+    }
+    display.push_for_release(0);
+    println!("");
+}
 
 fn main() {
     let port: u16;
@@ -43,21 +44,13 @@ fn main() {
             exit_with_usage(-1);
         }
     }
-    
+
     let listener = TcpListener::bind(("0.0.0.0", port)).unwrap();
     match listener.accept() {
         Ok((mut socket, addr)) => {
-          println!("connected: {addr:?}");
-          display_from(&mut socket);
-        },
+            println!("connected: {addr:?}");
+            display_from(&mut socket);
+        }
         Err(e) => println!("couldn't get client: {e:?}"),
     }
-    
-    // let mut output_dest = stdout().lock();
-    // let mut display = display::OutputDisplay::new(&mut output_dest);
-    // for q in rx_main {
-    //     display.push_for_release(q);
-    // }
-    // display.push_for_release(0);
-    // println!("");
 }
