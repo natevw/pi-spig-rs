7bb528617ac32124d71400fb745a7188db828493
Nathan Vander Wilt <nate@calftrail.com>
2025-03-14 17:19:31 -0700

    tweak args order and add testing notes

--- src/bin/worker.rs
+++ src/bin/worker.rs
@@ -3,40 +3,40 @@ use std::net::{TcpListener, TcpStream};
 use std::{env, process};
 
 fn exit_with_usage(code: i32) -> ! {
-    eprintln!("Usage: pi-spig-worker <offset> <size> <port> <dst_addr>");
+    eprintln!("Usage: pi-spig-worker <dst_addr> <port> <offset> <size>");
     process::exit(code);
 }
 
 fn main() {
+    let dst_addr: &str;
+    let src_port: u16;
     let data_offset: usize;
     let data_size: usize;
-    let src_port: u16;
-    let dst_addr: &str;
     let args: Vec<String> = env::args().skip(1).collect();
     match args.len() {
         4 => {
-            data_offset = match args[0].parse() {
-                Ok(n) => n,
-                Err(_) => {
-                    eprintln!("Expected integer offset");
-                    exit_with_usage(1);
-                }
-            };
-            data_size = match args[1].parse() {
-                Ok(n) => n,
-                Err(_) => {
-                    eprintln!("Expected integer size");
-                    exit_with_usage(1);
-                }
-            };
-            src_port = match args[2].parse() {
+            dst_addr = &args[0];
+            src_port = match args[1].parse() {
                 Ok(n) => n,
                 Err(_) => {
                     eprintln!("Expected integer port");
                     exit_with_usage(1);
                 }
             };
-            dst_addr = &args[3];
+            data_offset = match args[2].parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    eprintln!("Expected integer offset");
+                    exit_with_usage(1);
+                }
+            };
+            data_size = match args[3].parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    eprintln!("Expected integer size");
+                    exit_with_usage(1);
+                }
+            };
         }
         _ => {
             //print!("args: {:#?}", args);
